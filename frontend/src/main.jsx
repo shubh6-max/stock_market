678e6c7015266577f55c87f61ba9bda1fb2474d3
Shubham Puranik <puranikshubham87@gmail.com>
2026-06-28 05:00:19 +0530

    Import chart styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./styles.css";
 import "./mission-layout.css";
 import "./mode-tabs.css";
+import "./chart.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
