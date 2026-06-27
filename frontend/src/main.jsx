ddbfc794bf745e63ed5caf24efc86d12af96b08d
Shubham Puranik <puranikshubham87@gmail.com>
2026-06-28 04:43:58 +0530

    Import trading mode tab styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./mission-layout.css";
+import "./mode-tabs.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
